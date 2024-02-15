4143624f7ed43d809f3e4037f3a3fde6eaa9d83c
rmroot <rootrm@ornl.gov>
2024-02-15 08:38:54 -0600

    add free fontawesome icons

--- src/app/core-components/navbar/navbar.component.ts
+++ src/app/core-components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IconDefinition, faHome } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
-
+  faHome: IconDefinition = faHome;
 }
