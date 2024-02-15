403832e9e9dd56876d0f949e738bafeb7cdbae31
rmroot <rootrm@ornl.gov>
2024-02-15 08:42:03 -0600

    issue 29 all tests passing

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,10 +3,11 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './core-components/navbar/navbar.component';
 import { WelcomeComponent } from './core-components/welcome/welcome.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
+    imports: [RouterTestingModule, FontAwesomeModule],
     declarations: [AppComponent, NavbarComponent, WelcomeComponent]
   }));
 
