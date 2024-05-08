27f4fb9d8980f711ab090090c34a546609c6d2b3
rmroot <rootrm@ornl.gov>
2024-05-08 12:41:32 -0500

    all tests passing

--- src/app/setup-wizard/pre-visit/review-pre-visit-setup/company-kpis-summary/company-kpis-summary.component.spec.ts
+++ src/app/setup-wizard/pre-visit/review-pre-visit-setup/company-kpis-summary/company-kpis-summary.component.spec.ts
@@ -1,14 +1,34 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CompanyKpisSummaryComponent } from './company-kpis-summary.component';
+import { SetupWizardService } from 'src/app/setup-wizard/setup-wizard.service';
+import { IdbCompany, getNewIdbCompany } from 'src/app/models/company';
+import { IdbFacility, getNewIdbFacility } from 'src/app/models/facility';
+import { IdbAssessment, getNewIdbAssessment } from 'src/app/models/assessment';
+import { IdbContact } from 'src/app/models/contact';
+import { IdbProject } from 'src/app/models/project';
+import { BehaviorSubject } from 'rxjs';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { HelperPipesModule } from 'src/app/shared/helper-pipes/helper-pipes.module';
 
 describe('CompanyKpisSummaryComponent', () => {
   let component: CompanyKpisSummaryComponent;
   let fixture: ComponentFixture<CompanyKpisSummaryComponent>;
 
+  let setupWizardService: Partial<SetupWizardService> = {
+    company: new BehaviorSubject<IdbCompany>(getNewIdbCompany('')),
+    facility: new BehaviorSubject<IdbFacility>(getNewIdbFacility('', '')),
+    projects: new BehaviorSubject<Array<IdbProject>>([]),
+    contacts: new BehaviorSubject<Array<IdbContact>>([]),
+    assessments: new BehaviorSubject<Array<IdbAssessment>>([getNewIdbAssessment('', '', '')]),
+  };
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [CompanyKpisSummaryComponent]
+      imports: [ FontAwesomeModule, HelperPipesModule],
+      declarations: [CompanyKpisSummaryComponent],
+      providers: [
+        { provide: SetupWizardService, useValue: setupWizardService }
+      ]
     })
     .compileComponents();
     
