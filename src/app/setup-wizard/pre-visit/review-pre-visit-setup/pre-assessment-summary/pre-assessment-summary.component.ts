f0735585e20fae7947ed98d6e030bddf62edb590
rmroot <rootrm@ornl.gov>
2024-05-08 10:47:51 -0500

    add pre-assessments to review pre-visit

--- src/app/setup-wizard/pre-visit/review-pre-visit-setup/pre-assessment-summary/pre-assessment-summary.component.ts
+++ src/app/setup-wizard/pre-visit/review-pre-visit-setup/pre-assessment-summary/pre-assessment-summary.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { IconDefinition, faScrewdriverWrench, faToolbox, faUser } from '@fortawesome/free-solid-svg-icons';
+import { IdbAssessment } from 'src/app/models/assessment';
+import { IdbContact } from 'src/app/models/contact';
+import { SetupWizardService } from 'src/app/setup-wizard/setup-wizard.service';
+import { ProcessEquipment } from 'src/app/shared/constants/processEquipment';
 
 @Component({
   selector: 'app-pre-assessment-summary',
@@ -7,4 +12,22 @@ import { Component } from '@angular/core';
 })
 export class PreAssessmentSummaryComponent {
 
+
+  assessments: Array<IdbAssessment>;
+  visitDate: Date;
+  faToolbox: IconDefinition = faToolbox;
+  faScrewdriverWrench: IconDefinition = faScrewdriverWrench;
+  processEquipment: Array<ProcessEquipment>;
+  contacts: Array<IdbContact>;
+  faUser: IconDefinition = faUser;
+  constructor(private setupWizardService: SetupWizardService){
+
+  }
+
+  ngOnInit(){
+    this.assessments = this.setupWizardService.assessments.getValue();
+    this.visitDate = this.assessments[0].visitDate;
+    this.processEquipment = this.setupWizardService.facility.getValue().processEquipment;
+    this.contacts = this.setupWizardService.contacts.getValue();
+  }
 }
