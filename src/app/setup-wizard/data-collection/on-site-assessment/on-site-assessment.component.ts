ac5fc450a198a18aad8ee894e9d6710018728d7f
rmroot <rootrm@ornl.gov>
2024-05-30 15:33:15 -0500

    nav to manage assessments if no assessment found matching route

--- src/app/setup-wizard/data-collection/on-site-assessment/on-site-assessment.component.ts
+++ src/app/setup-wizard/data-collection/on-site-assessment/on-site-assessment.component.ts
@@ -41,7 +41,7 @@ export class OnSiteAssessmentComponent {
   assessmentIndex: number;
   onSiteVisit: IdbOnSiteVisit;
   onSiteVisitSub: Subscription;
-  constructor(private router: Router, private assessmentIdbService: AssessmentIdbService, 
+  constructor(private router: Router, private assessmentIdbService: AssessmentIdbService,
     private activatedRoute: ActivatedRoute,
     private contactIdbService: ContactIdbService,
     private onSiteVisitIdbService: OnSiteVisitIdbService
@@ -68,7 +68,7 @@ export class OnSiteAssessmentComponent {
       } else if (this.assessmentIndex == -1 && this.onSiteVisit.assessmentIds.length > 0) {
         this.navigateToOnSiteAssessment(this.onSiteVisit.assessmentIds[0]);
       } else if (!this.assessment) {
-        this.router.navigateByUrl('/setup-wizard');
+        this.router.navigateByUrl('/setup-wizard/data-collection/' + this.onSiteVisit.guid + '/manage-assessments');
       }
     });
   }
@@ -105,7 +105,7 @@ export class OnSiteAssessmentComponent {
     this.router.navigateByUrl('/setup-wizard/data-collection/' + this.onSiteVisit.guid + '/review-data-collection');
   }
 
-  goBack(){
+  goBack() {
     this.router.navigateByUrl('/setup-wizard/data-collection/' + this.onSiteVisit.guid + '/manage-assessments');
   }
 }
