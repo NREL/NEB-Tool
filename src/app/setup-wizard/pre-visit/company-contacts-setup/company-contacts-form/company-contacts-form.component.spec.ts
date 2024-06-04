961361a19b0f4eec0a4b37bd4fa5aa0c4bec46a4
rmroot <rootrm@ornl.gov>
2024-06-04 15:39:08 -0500

    all tests passing

--- src/app/setup-wizard/pre-visit/company-contacts-setup/company-contacts-form/company-contacts-form.component.spec.ts
+++ src/app/setup-wizard/pre-visit/company-contacts-setup/company-contacts-form/company-contacts-form.component.spec.ts
@@ -1,19 +1,38 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CompanyContactsFormComponent } from './company-contacts-form.component';
+import { ContactIdbService } from 'src/app/indexed-db/contact-idb.service';
+import { IdbContact, getNewIdbContact } from 'src/app/models/contact';
+import { BehaviorSubject } from 'rxjs';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 describe('CompanyContactsFormComponent', () => {
   let component: CompanyContactsFormComponent;
   let fixture: ComponentFixture<CompanyContactsFormComponent>;
 
+
+  let contactIdbService: Partial<ContactIdbService> = {
+    contacts: new BehaviorSubject<Array<IdbContact>>([]),
+    getContactByGuid: () => {
+      return getNewIdbContact('', '')
+    }
+  };
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [CompanyContactsFormComponent]
+      imports: [ReactiveFormsModule, FontAwesomeModule],
+      declarations: [CompanyContactsFormComponent],
+      providers: [
+        { provide: ContactIdbService, useValue: contactIdbService }
+      ]
     })
-    .compileComponents();
-    
+      .compileComponents();
+
     fixture = TestBed.createComponent(CompanyContactsFormComponent);
     component = fixture.componentInstance;
+    component.contactGuid = '';
+    component.accordionIndex = 0;
+    component.contactIndex = 0;
     fixture.detectChanges();
   });
 
