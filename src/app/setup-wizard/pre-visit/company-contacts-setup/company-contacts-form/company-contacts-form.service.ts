be5f595d64fa36d6fbf9a8dc78b93c55be3a078d
Roy <roy.li@nrel.gov>
2024-06-10 21:07:08 -0600

    form validation on company-contacts-phone number

--- src/app/setup-wizard/pre-visit/company-contacts-setup/company-contacts-form/company-contacts-form.service.ts
+++ src/app/setup-wizard/pre-visit/company-contacts-setup/company-contacts-form/company-contacts-form.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, MinLengthValidator, Validators, ValidatorFn, AbstractControl, ValidationErrors } from '@angular/forms';
 import { IdbContact } from 'src/app/models/contact';
 
 @Injectable({
@@ -11,14 +11,60 @@ export class CompanyContactsFormService {
 
   getFormFromIdbContact(contact: IdbContact): FormGroup {
     return this.formBuilder.group({
-      'name': [contact.name, [Validators.required]]
+      'name': [contact.name, [Validators.required]],
       //TODO: add form controls corresponding to form
+      'phone': [contact.phone, [this.phoneNumberValidator()]],
+      'email': [contact.email, [Validators.email, Validators.minLength(1), Validators.maxLength(255)]],
     });
   }
 
   updateIdbContactFromForm(contactForm: FormGroup, contact: IdbContact): IdbContact {
     contact.name = contactForm.controls['name'].value;
     //TODO: add all the properties that will get updated by the form
+    const phone = this.phoneNumberParser(contactForm.controls['phone'].value);
+    if (!phone) {
+      contact.phone = phone;
+    }
+    contact.email = contactForm.controls['email'].value;
     return contact;
+  }
+
+  // Parse phone number
+  phoneNumberParser(phoneInput: string): string {
+    const value = (phoneInput || '').trim();
+    if (!value) return '';
+    const isInternational: boolean = value.startsWith('+'); // Check if international number
+    const digits = value.replace(/\D/g, ''); // Remove all non-digit characters
+
+    if (isInternational) {
+      // All digits should be [10, 18]
+      if (digits.length < 10 || digits.length > 18) {
+        return '';
+      }
+      // Match the country code
+      const pattern: RegExp = /^\+(\d{1,3})\s*[\d()-]+/;
+      const match = value.match(pattern);
+      if (!match) return '';
+      const countryCode = match[1];
+      return '+' + countryCode + ' ' + digits.substring(countryCode.length - 1);
+    } else {
+      // Without country code, digits must be 10
+      if (digits.length !== 10) {
+        return '';
+      }
+      return "+1" + ' ' + digits;
+    }
+  }
+
+  // Custom validator for phone number
+  phoneNumberValidator(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const phone = this.phoneNumberParser(control.value);
+      if (phone) {
+        return null;
+      } else {
+        return {invalidPhoneNumber: true };
+      }
+    };
   }
 }
