86eaf61ee3a3a202ab309600834a6fe0e81b559e
Roy <roy.li@nrel.gov>
2024-06-12 08:34:23 -0600

    after reset database, validator types seem to work, revert email validator in html and ts

--- src/app/setup-wizard/pre-visit/company-contacts-setup/company-contacts-form/company-contacts-form.service.ts
+++ src/app/setup-wizard/pre-visit/company-contacts-setup/company-contacts-form/company-contacts-form.service.ts
@@ -15,8 +15,7 @@ export class CompanyContactsFormService {
       //TODO: add form controls corresponding to form
       'phone': [contact.phone, [this.phoneNumberValidator()]],
       // 'email': [contact.email, [Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$"), Validators.maxLength(255)]],
-      // 'email': [contact.email, [Validators.email, Validators.maxLength(255)]],
-      'email': [contact.email, [Validators.email]],
+      'email': [contact.email, [Validators.email, Validators.maxLength(255)]],
       'role': [contact.role, []],
       'team': [contact.team, []],
       'focusArea': [contact.focusArea, []],
