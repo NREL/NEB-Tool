382065f9a19e3ecd600d84f16982adeb66d16f61
rmroot <rootrm@ornl.gov>
2024-06-28 09:11:22 -0500

    placeholder content for report rollup

--- src/app/setup-wizard/data-evaluation/visit-report/visit-report.component.ts
+++ src/app/setup-wizard/data-evaluation/visit-report/visit-report.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { IconDefinition, faChevronLeft, faFolderOpen } from '@fortawesome/free-solid-svg-icons';
+import { OnSiteVisitIdbService } from 'src/app/indexed-db/on-site-visit-idb.service';
+import { IdbOnSiteVisit } from 'src/app/models/onSiteVisit';
 
 @Component({
   selector: 'app-visit-report',
@@ -7,4 +11,25 @@ import { Component } from '@angular/core';
 })
 export class VisitReportComponent {
 
+  faChevronLeft: IconDefinition = faChevronLeft;
+  faFolderOpen: IconDefinition = faFolderOpen;
+
+  onSiteVisit: IdbOnSiteVisit;
+  constructor(private router: Router,
+    private onSiteVisitIdbService: OnSiteVisitIdbService
+  ) {
+
+  }
+
+  ngOnInit() {
+    this.onSiteVisit = this.onSiteVisitIdbService.selectedVisit.getValue();
+  }
+
+  goNext() {
+    this.router.navigateByUrl('/facility/' + this.onSiteVisit.facilityId);
+  }
+
+  goBack() {
+    this.router.navigateByUrl('/setup-wizard/data-collection/' + this.onSiteVisit.guid + '/review-data-collection');
+  }
 }
