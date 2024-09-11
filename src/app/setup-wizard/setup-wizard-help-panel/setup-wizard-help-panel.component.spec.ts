70e55640b5785713cd04038e4e07b333cbc6d381
rmroot <rootrm@ornl.gov>
2024-09-11 09:08:59 -0500

    add local storage to save help panel open

--- src/app/setup-wizard/setup-wizard-help-panel/setup-wizard-help-panel.component.spec.ts
+++ src/app/setup-wizard/setup-wizard-help-panel/setup-wizard-help-panel.component.spec.ts
@@ -2,15 +2,23 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SetupWizardHelpPanelComponent } from './setup-wizard-help-panel.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { LocalStorageService } from 'ngx-webstorage';
 
 describe('SetupWizardHelpPanelComponent', () => {
   let component: SetupWizardHelpPanelComponent;
   let fixture: ComponentFixture<SetupWizardHelpPanelComponent>;
+  let localStorageService: Partial<LocalStorageService> = {
+    retrieve: () => { return undefined },
+    store: () => { return undefined },
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [FontAwesomeModule],
-      declarations: [SetupWizardHelpPanelComponent]
+      declarations: [SetupWizardHelpPanelComponent],
+      providers: [
+        { provide: LocalStorageService, useValue: localStorageService }
+      ]
     })
     .compileComponents();
     
