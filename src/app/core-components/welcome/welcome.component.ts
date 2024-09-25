66aeeef8c0793127b16c6c03f18408785e93e3d9
rmroot <rootrm@ornl.gov>
2024-09-25 15:57:35 -0500

    first pass at updating welcome screen

--- src/app/core-components/welcome/welcome.component.ts
+++ src/app/core-components/welcome/welcome.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
-import { IconDefinition, faChevronRight, faFolderOpen, faWandMagicSparkles } from '@fortawesome/free-solid-svg-icons';
+import { IconDefinition, faBookOpen, faBookReader, faBuilding, faChevronRight, faDatabase, faExternalLink, faFolderOpen, faIndustry, faQuestionCircle, faSearchPlus, faStopwatch, faWandMagicSparkles } from '@fortawesome/free-solid-svg-icons';
+import { Subscription } from 'rxjs';
+import { CompanyIdbService } from 'src/app/indexed-db/company-idb.service';
+import { FacilityIdbService } from 'src/app/indexed-db/facility-idb.service';
+import { OnSiteVisitIdbService } from 'src/app/indexed-db/on-site-visit-idb.service';
 import { UserIdbService } from 'src/app/indexed-db/user-idb.service';
+import { IdbCompany } from 'src/app/models/company';
+import { IdbFacility } from 'src/app/models/facility';
+import { IdbOnSiteVisit } from 'src/app/models/onSiteVisit';
 import { IdbUser } from 'src/app/models/user';
 import { SharedDataService } from 'src/app/shared/shared-services/shared-data.service';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-welcome',
@@ -14,16 +22,56 @@ export class WelcomeComponent {
   faChevronRight: IconDefinition = faChevronRight;
   faWandMagicSparkles: IconDefinition = faWandMagicSparkles;
   faFolderOpen: IconDefinition = faFolderOpen;
+  faBookOpen: IconDefinition = faBookOpen;
+  faExternalLink: IconDefinition = faExternalLink
+  faQuestionCircle: IconDefinition = faQuestionCircle;
+  faDatabase: IconDefinition = faDatabase;
+  faBuilding: IconDefinition = faBuilding;
+  faIndustry: IconDefinition = faIndustry;
+  faSearchPlus: IconDefinition = faSearchPlus;
+  faStopwatch: IconDefinition = faStopwatch
+
 
   user: IdbUser;
+
+  onSiteVisits: Array<IdbOnSiteVisit>;
+  onSiteVisitSub: Subscription;
+
+  facilities: Array<IdbFacility>;
+  facilitiesSub: Subscription;
+
+  companies: Array<IdbCompany>;
+  companiesSub: Subscription;
   constructor(private userIdbService: UserIdbService,
-    private sharedDataService: SharedDataService
+    private sharedDataService: SharedDataService,
+    private onSiteVisitIdbService: OnSiteVisitIdbService,
+    private facilityIdbService: FacilityIdbService,
+    private companyIdbService: CompanyIdbService
   ) {
 
   }
 
   ngOnInit() {
     this.user = this.userIdbService.user.getValue();
+    this.onSiteVisitSub = this.onSiteVisitIdbService.onSiteVisits.subscribe(visits => {
+      this.onSiteVisits =  _.orderBy(visits, (visit: IdbOnSiteVisit) => {
+        return new Date(visit.modifiedDate);
+      }, 'desc').slice(0, 5);
+    });
+
+    this.facilitiesSub = this.facilityIdbService.facilities.subscribe(facilities => {
+      this.facilities = facilities;
+    });
+
+    this.companiesSub = this.companyIdbService.companies.subscribe(companies => {
+      this.companies = companies;
+    })
+  }
+
+  ngOnDestroy(){
+    this.onSiteVisitSub.unsubscribe();
+    this.facilitiesSub.unsubscribe();
+    this.companiesSub.unsubscribe();
   }
 
   async saveChanges() {
@@ -31,6 +79,13 @@ export class WelcomeComponent {
   }
 
   openWizardModal() {
+    this.sharedDataService.createAssessmentModalOpen.next(true);
+  }
+
+  goToVisit(visit: IdbOnSiteVisit) {
+    this.companyIdbService.setSelectedFromGUID(visit.companyId);
+    this.facilityIdbService.setSelectedFromGUID(visit.facilityId);
+    this.onSiteVisitIdbService.setSelectedFromAssessmentGUID(visit.assessmentIds[0]);
     this.sharedDataService.createAssessmentModalOpen.next(true);
   }
 }
